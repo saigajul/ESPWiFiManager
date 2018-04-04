ec82815166d028181d7369f81d4bfdfef088dccb
saigajul <36195719+saigajul@users.noreply.github.com>
2018-04-04 22:21:43 +0530

    Add files via upload

---
+++ ESPWiFiManager/ESPWiFiManager.ino
@@ -0,0 +1,32 @@
+/***********************************************************************************************************/
+// To save credential in AP mode        http://192.168.4.1/wifisave?s=********&p=********/
+/***********************************************************************************************************/
+// To reset credential in AP mode       http://192.168.4.1/r
+/***********************************************************************************************************/
+// To reset credential in station mode  http://xxx.xxx.xxx.xxx/your-text
+/***********************************************************************************************************/
+
+#include <ESPWiFiManager.h>
+
+ESP8266WebServer server(80);
+ESPWiFiManager MyESP;
+
+void resthandle()
+{
+    server.send(200, "text/plain", "Restoring to default");
+    MyESP.handleReset();
+}
+
+void setup()
+{
+    Serial.begin(115200);    
+    MyESP.autoConnect("ESPWiFiManager");
+    server.on("/reset",resthandle);
+    server.begin();
+}
+
+void loop()
+{
+    server.handleClient();
+    delay(1);
+} 
